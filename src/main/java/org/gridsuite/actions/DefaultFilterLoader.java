8aeb365b89a472d25774bc5f1d4e59a6bdd0afd9
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-02-12 08:51:50 +0100

    PR remarks
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/main/java/org/gridsuite/actions/DefaultFilterLoader.java
+++ src/main/java/org/gridsuite/actions/DefaultFilterLoader.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.actions;
 
 import org.gridsuite.filter.AbstractFilter;
@@ -6,6 +12,9 @@ import org.gridsuite.filter.FilterLoader;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * @author Kevin Le Saulnier <kevin.le-saulnier at rte-france.com>
+ */
 public class DefaultFilterLoader implements FilterLoader {
 
     private final FilterProvider filterProvider;
--- src/test/java/org/gridsuite/actions/ContingencyListEvaluatorTest.java
+++ src/test/java/org/gridsuite/actions/ContingencyListEvaluatorTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.actions;
 
 import com.powsybl.contingency.ContingencyElement;
@@ -31,6 +37,9 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
+/**
+ * @author Kevin Le Saulnier <kevin.le-saulnier at rte-france.com>
+ */
 class ContingencyListEvaluatorTest {
     @Test
     void testEvaluateFilterBasedContingencyList() {
