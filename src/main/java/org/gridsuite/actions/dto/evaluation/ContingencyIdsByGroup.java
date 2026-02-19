06ea012c075339da3622939e371734d64296a526
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-02-19 13:16:29 +0100

    add annotation to dto to make it work with jackson
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/main/java/org/gridsuite/actions/dto/evaluation/ContingencyIdsByGroup.java
+++ src/main/java/org/gridsuite/actions/dto/evaluation/ContingencyIdsByGroup.java
@@ -6,8 +6,10 @@
  */
 package org.gridsuite.actions.dto.evaluation;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.Map;
@@ -18,6 +20,8 @@ import java.util.UUID;
  */
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContingencyIdsByGroup {
     Map<String, List<UUID>> ids;
 }
